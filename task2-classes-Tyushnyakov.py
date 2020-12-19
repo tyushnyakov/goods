d3e16d230c4767de06ff4d29018ee29985d8e3a1
tyushnyakov <ktyushnyakov@gmail.com>
2020-12-19 11:19:56 +1000

    added add_from_file method, corrected code

--- task2-classes-Tyushnyakov.py
+++ task2-classes-Tyushnyakov.py
@@ -17,128 +17,182 @@ class GoodInfo:
     :type cost: float
     """
 
-    def __init__(self, name, count, cost):
+    def __init__(self, name, cost, count):
         self.name = name
-        self.count = count
         self.cost = cost
+        self.count = count
+
+    def __str__(self):
+        return "Товар:{name} Цена: {cost} Количество: {count}"\
+            .format(name=self.name, cost=self.cost, count=self.count)
 
 
 class GoodInfoList:
     """
-        Processes list of goods with name, quantity and price properties.
+    Processes list of goods with name, quantity and price properties.
 
-        Realizes methods: get most expensive goods; get cheapest goods;
-        get end product list; sort goods list by the name, count or cost;
-        add product; remove product; remove most expensive product;
-        get product by the index; get standard deviation of prices;
-        remove last product.
+    Realizes methods: get most expensive goods; get cheapest goods;
+    get end product list; sort goods list by the name, count or cost;
+    add product; remove product; remove most expensive product;
+    get product by the index; get standard deviation of prices;
+    remove last product.
 
-        :param goods: list of goods
-        :type goods: list
-        """
+    :param goods: list of goods
+    :type goods: list
+    """
 
-    def __init__(self, goods):
+    def __init__(self, goods=[]):
         self.goods = goods
 
-    def __getitem__(self, goods_number):
-        return self.goods[goods_number]
+    def __str__(self):
+        return '\n'.join(str(item) for item in self.goods)
+
+    def __getitem__(self, index):
+        return self.goods[index]
 
     def __len__(self):
         return len(self.goods)
 
+    def add_from_file(self, data_file):
+        """
+        This method gets goods data from the text file, transform text data
+
+        into new GoodInfo objects and add them to the GoodInfoList
+
+        :param data_file: path to file
+        :type data_file: string
+        """
+        open_file = open(data_file, "r", encoding="utf-8")
+        rows = open_file.readlines()
+        open_file.close()
+
+        for row in rows:
+            list_row = row.split(":")
+            list_row[2] = list_row[2].replace("\n", "")
+            if len(list_row) < 3:
+                print("Нет данных о товаре")
+            elif any(x.name == list_row[0] for x in self.goods):
+                print("Такой товар уже есть!")
+            elif not list_row[1].isdigit() or not list_row[2].isdigit():
+                print("Неверный формат данных")
+            name = list_row[0]
+            cost = float(list_row[1])
+            count = int(list_row[2])
+            self.add(GoodInfo(name, cost, count))
+
+    def get_mean(self):
+        """
+        This method calculates mean price of goods in list
+
+        :return: mean price of goods in list
+        :rtype: float
+        """
+        return sum([item.cost for item in self.goods]) / len(self.goods)
+
     def get_std(self):
+        """
+        This method calculates standard deviation of product prices
+
+        :return: standard deviation of product prices
+        :rtype: float
+        """
         return stdev([x.cost for x in self.goods])
 
     def sort_goods(self, sort_key):
+        """
+        This method sort list by the name, count or cost
+
+        :param sort_key: sort key as string
+        :type sort_key: str
+        :return: sorted list of goods
+        :rtype: list
+        """
         if sort_key == 'name':
             self.goods.sort(key=lambda x: x.name)
-        if sort_key == 'count':
-            self.goods.sort(key=lambda x: x.count)
         if sort_key == 'cost':
             self.goods.sort(key=lambda x: x.cost)
+        if sort_key == 'count':
+            self.goods.sort(key=lambda x: x.count)
         return self
 
     def get_expensive(self):
+        """
+        This method get most expensive goods from the list
+
+        :return: most expensive goods list
+        :rtype: list
+        """
         self.sort_goods('cost')
-        return self.goods[-3:]
+        highest_cost = self.goods[-1].cost
+        goods = [x for x in self.goods if x.cost == highest_cost]
+        return GoodInfoList(goods)
 
     def get_cheapest(self):
+        """
+        This method get cheapest goods from the list
+
+        :return: cheapest goods list
+        :rtype: list
+        """
         self.sort_goods('cost')
-        return self.goods[:3]
+        lowest_cost = self.goods[0].cost
+        goods = [x for x in self.goods if x.cost == lowest_cost]
+        return GoodInfoList(goods)
 
     def get_ending(self):
+        """
+        This method get ending goods from the list
+
+        :return: ending goods list
+        :rtype: list
+        """
         self.sort_goods('count')
-        return self.goods[:3]
+        minimal_count = self.goods[0].count
+        goods = [x for x in self.goods if x.count == minimal_count]
+        return GoodInfoList(goods)
 
     def add(self, product):
+        """
+        This method add product to the goods list
+
+        :param product: product to add as instance of GoodInfo
+        :type product: object
+        """
         self.goods.append(product)
 
     def remove(self, name):
+        """
+        This method remove product from the goods list
+
+        :param name: product name to remove
+        :type name: str
+        """
         self.goods = [item for item in self.goods if item.name != name]
 
     def remove_expensive(self):
+        """
+        This method remove most expensive product from the goods list
+
+        :return: most expensive product from the goods list
+        :rtype: object
+        """
         self.sort_goods('cost')
-        self.goods.pop()
+        return self.goods.pop()
 
     def remove_last(self):
-        self.goods.pop()
+        """
+        This method remove last product from the goods list
+
+        :return: last product from the goods list
+        :rtype: object
+        """
+        return self.goods.pop()
 
 
-def get_data(data_file):
-    """
-    This function gets data from the text file and returns list of goods
+goods_list = GoodInfoList()
+goods_list.add_from_file('goods')
 
-    :param data_file: path to file
-    :type data_file: string
-    :return: returns list of GoodInfo objects
-    :rtype: list of objects
-    """
-    open_file = open(data_file, "r", encoding="utf-8")
-    string_list = open_file.readlines()
-    open_file.close()
-
-    goods_list = []
-    for string in string_list:
-        list_from_string = string.split(":")
-        goods_list.append(create_good_info(goods_list, list_from_string))
-
-    return goods_list
-
-
-def create_good_info(good_info_list, item_list):
-    """
-    This function transforms string row to instance of GoodInfo object
-
-    :param good_info_list: processed list of GoodInfo objects
-    :type good_info_list: list of objects
-    :param item_list: row from data file transformed to list of strings
-    :type item_list: list of strings
-    :return: new GoodInfo object
-    """
-    item_list[2] = item_list[2].replace("\n", "")
-    if len(item_list) < 3:
-        print("Нет данных о товаре")
-    elif any(x.name == item_list[0] for x in good_info_list):
-        print("Такой товар уже есть!")
-    elif not item_list[1].isdigit() or not item_list[2].isdigit():
-        print("Неверный формат данных")
-
-    return GoodInfo(item_list[0], int(item_list[1]), float(item_list[2]))
-
-
-goods = get_data("goods")
-# total - total count of goods
-total = len(goods)
-# mean - mean price of goods
-mean = sum([item.cost for item in goods]) / total
-# max_price - maximal price good
-max_price = GoodInfoList(goods).get_expensive()[2]
-# min_count - minimal count good
-min_count = GoodInfoList(goods).get_ending()[0]
-
-print("Общее количество товаров - {total}".format(total=total))
-print("Средняя цена товара - {mean:.2f}".format(mean=mean))
-print("Самый дорогой товар - {name}, Цена\
- - {price}".format(name=max_price.name, price=max_price.cost))
-print("Заканчивается товар - {name}, Осталось\
- - {count}".format(name=min_count.name, count=min_count.count))
+print("Общее количество товаров - {total}".format(total=len(goods_list)))
+print("Средняя цена товара - {mean:.2f}".format(mean=goods_list.get_mean()))
+print("Самые дорогие товары - \n {exp}".format(exp=goods_list.get_expensive()))
+print("Заканчивются товары - \n {end}".format(end=goods_list.get_ending()))
