0bb689df9888bd38189871e991878ba7c02b9bbb
tyushnyakov <ktyushnyakov@gmail.com>
2020-12-28 17:58:23 +1000

    tests setup

--- good_info_tests.py
+++ good_info_tests.py
@@ -4,148 +4,107 @@ import unittest
 
 class GoodInfoTest(unittest.TestCase):
 
+    def setUp(self):
+        self.goods_list = GoodInfoList([])
+        self.goods_list.add_from_file('goods-test.info')
+
     def test_total(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(len(goods_list), 46)
+        self.assertEqual(len(self.goods_list), 46)
 
     def test_different(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(len(goods_list['морковь 1кг']), 2)
+        self.assertEqual(len(self.goods_list['морковь 1кг']), 2)
 
     def test_mean(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(goods_list.get_mean(), 98.50)
+        self.assertEqual(self.goods_list.get_mean(), 98.50)
 
     def test_std(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(goods_list.get_std(), 95.37965075307089)
+        self.assertEqual(self.goods_list.get_std(), 95.37965075307089)
 
     def test_expensive(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(str(goods_list.get_expensive()),
-                         'Товар:рыба мороженая, Кета 1кг Цена: 400.0 '
-                         'Количество: 5 Произведен:2020-12-30 '
-                         'Поставка: 2020-12-30 Срок годности 90 дней')
+        self.assertEqual(self.goods_list.get_expensive()[0],
+                         GoodInfo('рыба мороженая, Кета 1кг', 400.0,
+                                  5, '2020-12-30', '2020-12-30', 90))
 
     def test_ending(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(goods_list.get_ending()[0],
+        self.assertEqual(self.goods_list.get_ending()[0],
                          GoodInfo('пирожки с картошкой', 30.0, 2,
                                   '2020-12-30', '2020-12-30', 7))
 
     def test_sort(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(goods_list.sort_goods('name')[0],
+        self.assertEqual(self.goods_list.sort_goods('name')[0],
                          GoodInfo('Чай зеленый Lipton 10 пак.', 60.0,
                                   20, '2020-12-30', '2020-12-30',1080))
 
     def test_without_name(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
         with self.assertRaises(TypeError):
-            goods_list.add(GoodInfo(10, 10, '2020-12-30', '2020-12-30', 30))
+            self.goods_list.add(GoodInfo(10, 10, '2020-12-30', '2020-12-30', 30))
 
     def test_wrong_expiration(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.add(GoodInfo('хлеб', 10, 10,
+        self.goods_list.add(GoodInfo('хлеб', 10, 10,
                                 '2020-12-30', '2020-12-30', -30))
-        self.assertNotEqual(goods_list[-1],
+        self.assertNotEqual(self.goods_list[-1],
                             GoodInfo('хлеб', 10, 10,
                                      '2020-12-30', '2020-12-30', -30))
 
     def test_add_expired(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.add(GoodInfo('хлеб', 10, 10,
+        self.goods_list.add(GoodInfo('хлеб', 10, 10,
                                 '2020-12-20', '2020-12-30', 3))
-        self.assertNotEqual(goods_list[-1],
+        self.assertNotEqual(self.goods_list[-1],
                             GoodInfo('хлеб', 10, 10,
                                      '2020-12-20', '2020-12-30', 3))
 
     def test_negative_cost(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.add(GoodInfo('хлеб', -10, 10,
+        self.goods_list.add(GoodInfo('хлеб', -10, 10,
                                 '2020-12-30', '2020-12-30', 30))
-        self.assertNotEqual(goods_list[-1],
+        self.assertNotEqual(self.goods_list[-1],
                             GoodInfo('хлеб', -10, 10,
                                      '2020-12-30', '2020-12-30', 30))
 
     def test_negative_count(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.add(GoodInfo('хлеб', 10, -10,
+        self.goods_list.add(GoodInfo('хлеб', 10, -10,
                                 '2020-12-30', '2020-12-30', 30))
-        self.assertNotEqual(goods_list[-1],
+        self.assertNotEqual(self.goods_list[-1],
                             GoodInfo('хлеб', 10, -10,
                                      '2020-12-30', '2020-12-30', 30))
 
     def test_zero_count(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.add(GoodInfo('хлеб', 10, 0, '2020-12-30',
+        self.goods_list.add(GoodInfo('хлеб', 10, 0, '2020-12-30',
                                 '2020-12-30', 30))
-        self.assertNotEqual(goods_list[-1],
+        self.assertNotEqual(self.goods_list[-1],
                             GoodInfo('хлеб', 10, 0, '2020-12-30',
                                      '2020-12-30', 30))
 
     def test_remove(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.remove('свинина 1кг')
-        self.assertFalse(goods_list['свинина 1кг'])
+        self.goods_list.remove('свинина 1кг')
+        self.assertFalse(self.goods_list['свинина 1кг'])
 
     def test_remove_expensive(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(str(goods_list.remove_expensive()),
+        self.assertEqual(str(self.goods_list.remove_expensive()),
                          'Товар:рыба мороженая, Кета 1кг Цена: 400.0 '
                          'Количество: 5 Произведен:2020-12-30 '
                          'Поставка: 2020-12-30 Срок годности 90 дней')
 
     def test_sell_one(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(goods_list.sell('говядина 1кг', 1), 250)
+        self.assertEqual(self.goods_list.sell('говядина 1кг', 1), 250)
 
     def test_sell_more(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertFalse(goods_list.sell('говядина 1кг', 100))
+        self.assertFalse(self.goods_list.sell('говядина 1кг', 100))
 
     def test_sell_not_exist(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertFalse(goods_list.sell('черешня', 1))
+        self.assertFalse(self.goods_list.sell('черешня', 1))
 
     def test_sell_absent(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        goods_list.sell('баранина 1кг', 10)
-        self.assertFalse(goods_list.sell('баранина 1кг', 10))
+        self.goods_list.sell('баранина 1кг', 10)
+        self.assertFalse(self.goods_list.sell('баранина 1кг', 10))
 
     def test_read_file(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertEqual(len(goods_list), 46)
+        self.assertEqual(len(self.goods_list), 46)
 
     def test_read_empty(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertNotEqual(str(goods_list[-2]), '')
+        self.assertNotEqual(str(self.goods_list[-2]), '')
 
     def test_read_colons(self):
-        goods_list = GoodInfoList([])
-        goods_list.add_from_file('goods2.info')
-        self.assertNotEqual(str(goods_list[-1]),
+        self.assertNotEqual(str(self.goods_list[-1]),
                             'Товар: Цена: Количество: Произведен: '
                             'Поставка: Срок годности  дней')
 
--- reporter.py
+++ reporter.py
@@ -8,35 +8,29 @@ logging.basicConfig(filename="goods.log", filemode='a', level=logging.INFO, form
 
 
 def main():
-    # logging.info('Program started')
-    #
-    # file_path = os.path.abspath(input('Введите путь к файлу:').strip('"\''))
-    # if not os.path.exists(file_path):
-    #     print('Такого файла нет')
-    #     logging.error('Нет файла {}'.format(file_path))
-    # elif not os.path.isfile(file_path):
-    #     print('Зто не файл')
-    #     logging.error('{} - не файл'.format(file_path))
-    # else:
-    #     shutil.copy(file_path, 'data')
+    logging.info('Program started')
+
+    file_path = os.path.abspath(input('Введите путь к файлу:').strip('"\''))
+    if not os.path.exists(file_path):
+        print('Такого файла нет')
+        logging.error('Нет файла {}'.format(file_path))
+    elif not os.path.isfile(file_path):
+        print('Зто не файл')
+        logging.error('{} - не файл'.format(file_path))
+    else:
+        shutil.copy(file_path, 'data')
         goods_list = GoodInfoList()
-        goods_list.add_from_file('goods2.info')
+        goods_list.add_from_file(file_path)
 
         print("Общее количество товаров - {total}".format(total=len(goods_list)))
-        # print("Средняя цена товара - {mean:.2f}".format(mean=goods_list.get_mean()))
-        # print(goods_list.get_std())
-        # print("Самые дорогие товары - \n {exp}".format(exp=goods_list.get_expensive()))
-        # print("Заканчивются товары - \n {end}".format(end=goods_list.get_ending()))
-        # print("Просроченные товары - \n {}".format(goods_list.get_expired()))
-        # print(goods_list['свинина 1 кг'])
-        # logging.info('Program finished')
-        # print(goods_list.sell('морковь 1кг', 60))
-        # print(goods_list.revenue)
-        # print(goods_list['морковь 1кг'])
-        # goods_list.add(GoodInfo('хлеб', 10, 10, '2020-12-20', '2020-12-30', 30))
-        # print(goods_list[-1])
-        # print(goods_list.remove_expensive())
-        print(goods_list.get_ending())
+        print("Средняя цена товара - {mean:.2f}".format(mean=goods_list.get_mean()))
+        print(goods_list.get_std())
+        print("Самые дорогие товары - \n {exp}".format(exp=goods_list.get_expensive()))
+        print("Заканчивются товары - \n {end}".format(end=goods_list.get_ending()))
+        print("Просроченные товары - \n {}".format(goods_list.get_expired()))
+        print(goods_list['свинина 1 кг'])
+
+    logging.info('Program finished')
 
 
 if __name__ == '__main__':
